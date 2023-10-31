286e8b383b5ae7363d973a67cde2c8689b942407
Pikakyi <149421108+Pikakyi@users.noreply.github.com>
2023-10-31 12:00:07 -0400

    Update UI.py
    Created deck

--- UI.py
+++ UI.py
@@ -1,1 +1,10 @@
-print('test')
+suits = ['♠', '♣', '♢', '♡']
+numbers = [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
+deck = []
+
+for suit in suits:
+    for num in numbers:
+        deck.append([num, suit])
+
+print(deck)
+
