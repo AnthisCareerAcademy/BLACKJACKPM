40181ba211c1910e03b1162d03766d218455df2d
Pikakyi <149421108+Pikakyi@users.noreply.github.com>
2023-10-31 12:36:03 -0400

    Update UI.py
    added inputs

--- UI.py
+++ UI.py
@@ -1,5 +1,8 @@
 print("Welcome to BlackJack")
 input("Would you please state your name: ")
+input("Would you please state your age: ")
+print("The rule for this game is to get close to 21 or get to 21. ")
+
 suits = ['♠', '♣', '♢', '♡']
 numbers = [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
 deck = []
