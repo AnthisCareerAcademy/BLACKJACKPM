5ea21a22683d3c3324a4ddca4707810dad336062
Pikakyi <149421108+Pikakyi@users.noreply.github.com>
2023-10-31 12:49:49 -0400

    Update UI.py
    shuffle + visual card

--- UI.py
+++ UI.py
@@ -6,5 +6,23 @@ for suit in suits:
     for num in numbers:
         deck.append([num, suit])
 
-print(deck)
 
+
+import random
+random.shuffle(deck)
+print(deck)
+#visual
+print(f"""          
+          ________           ________
+         |        |         |{deck[0][1]}       |
+         |        |         |        |
+         |   ?    |         |   {deck[0][0]}    |
+         |        |         |        |
+         |________|         |_______{deck[0][1]}|
+          ________           ________
+         |{deck[1][1]}       |         | {deck[2][0]}      |
+         |        |         |        |
+         |   {deck[1][0]}    |         |   {deck[2][1]}    |
+         |        |         |        | 
+         |_______{deck[1][1]}|         |_______{deck[2][0]}|
+              """)
